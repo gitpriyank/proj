96972401d839dcc1866b18f645f449bf1bc02d52
vppriyank <pverma7525@gmail.com>
2017-06-21 15:31:57 +0530

    First commit

---
+++ tcpserver.py
@@ -0,0 +1,23 @@
+import socket
+
+def main():
+	host='127.0.0.1'
+	port=5000
+	s=socket.socket()
+	s.bind((host, port))
+	s.listen(1)
+	c, addr= s.accept()
+	print("Connection from: " + str(addr))
+
+	while True:
+		data = c.recv(1024).decode('utf-8')
+		if not data:
+			break
+		print("Received from client: " + data)
+		data = data.upper()
+		c.send(data.encode('utf-8'))
+
+	c.close()
+
+if __name__ == "__main__":
+	main()
